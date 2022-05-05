c4fb2d1ee7749315d00040d6b0cb019bfe3b4cc7
rose <rose_j98@yahoo.ca>
2022-05-04 17:14:49 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Weather App</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <Weather />
+          <footer>
+            This project created by R.J{" "}
+            <a
+              href="https://github.com/rosej98/react-weather-apps"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open Source github
+            </a>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city name"
+              className="form-control"
+              autoFocus="on"
+            ></input>
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary"
+            ></input>
+          </div>
+        </div>
+      </form>
+
+      <div className="row">
+        <div className="col-7">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="partly-cloudy"
+          ></img>
+
+          <span className="temp">17</span>
+          <span className="unit">°C|°F</span>
+
+          <ul className="details">
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 76%</li>
+            <li>Wind: 6 km/h</li>
+          </ul>
+        </div>
+        <div className="col-5">
+          <h1>New York</h1>
+          <ul>
+            <li>Wednesday</li>
+            <li>Mostly Cloudy</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
